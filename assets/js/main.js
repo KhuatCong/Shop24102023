076727c9dafe05fc032c973ffc78d0812bf015ee
KhuatCong <khuatphucong06092002@gmail.com>
2023-10-24 14:27:43 +0700

    hehehe

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,47 @@ $(function () {
         slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        prevArrow: $('.prev'),
-        nextArrow: $('.next'),
+        prevArrow: $('.prev-banner'),
+        nextArrow: $('.next-banner'),
+    });
+})
+
+$(function () {
+    $('.list-sale').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        prevArrow: $('.prev-sale'),
+        nextArrow: $('.next-sale'),
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                }
+              },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
     });
 })
